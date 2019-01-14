66e43ad0abc1f615bc14c05c233924aa80b59a23
giorgioriccardi <griccardi@my.bcit.ca>
2019-01-14 10:43:29 -0800

    aleee-03 version

--- dress-size.js
+++ dress-size.js
@@ -1,170 +1,76 @@
 jQuery(document).ready(function($) {
     console.log("ready!");
+
+    var sizes = [
+      {"bodypart": "bust", "IN":[34,36,38,40], "CM": [86,91,96,102]},
+      {"bodypart": "waist", "IN":[26,28,30,32], "CM": [66,71,76,81]},
+      {"bodypart": "hips", "IN":[36.5,38.5,40.5, 42.5], "CM": [93,98,104,108]}
+    ];
+
     var womanEmpty = $("#sc-woman-empty");
     var womanWaist = $("#sc-woman-waist");
     var womanBust = $("#sc-woman-bust");
     var womanHips = $("#sc-woman-hips");
+    var sizeTableBody  = $("#sc-size-table-body");
 
-    var steps = new Array();
-    var stepsContent = new Array();
-    var stepsHint = new Array();
+    var unit = "IN";
+    var unitLabels = {"IN":"in", "CM": "cm"};
+
+
+
+    var unitToggle = $("#sc-unit-toggle");
+
+    unitToggle.change(function() {
+      console.log("c",this.checked);
+      unit = this.checked?"CM":"IN";
+      refreshTable();
+    });
+
+    var selectedSizeCell = null;
+    var selectedImage = womanEmpty;
+    var data_th = ['XS','S','M','L'];
+    var refreshTable = function(){
+      var tbody = "";
+      for(var i=0; i<sizes.length;i++){
+        tbody += '<tr  id="sc-row-'+sizes[i].bodypart+'"><td class="sc-bodypart-cell">'+sizes[i].bodypart+'</td>';
+        for(var j=0; j<sizes[i][unit].length;j++)
+          tbody += '<td data-th="'+data_th[j]+'" id="sc_cell_'+i+'_'+j+'">'+sizes[i][unit][j]+'</td>';
+        tbody += ' </td></tr>';
+      }
+
+      sizeTableBody.html(tbody);
+      $("#sc-row-waist").hover(function () {  selectedImage.hide(); womanWaist.show(); },
+      function () { womanWaist.hide(); selectedImage.show(); });
+      $("#sc-row-bust").hover(function () {  selectedImage.hide(); womanBust.show();},
+        function () { womanBust.hide(); selectedImage.show(); });
+      $("#sc-row-hips").hover(function () { selectedImage.hide(); womanHips.show();},
+        function () { womanHips.hide();  selectedImage.show(); });
+
+        $("#sc-size-table-body td").click(function(e){
+          $("#sc-size-table-body td").each(function(){
+            $(this).removeClass("size-selected");
+          });
+          $(this).addClass("size-selected");
+          selectedSizeCell = $(this).attr("id");
     
-    for( i=1; i<4; i++){
-      steps.push($("#sc-size-calc-step-"+i));
-      stepsContent.push($("#sc-size-calc-content-"+i));
-      stepsHint.push($("#sc-size-calc-hint-"+i));
-    }
-
-    var stepsResultHint= $("#sc-size-calc-result-hint");
-    var calcBodypart = {"sc-size-calc-bodypart-waist": $("#sc-size-calc-bodypart-waist"), 
-    "sc-size-calc-bodypart-bust": $("#sc-size-calc-bodypart-bust"),
-    "sc-size-calc-bodypart-hips": $("#sc-size-calc-bodypart-hips")};
-
-    var calcBodypartSeparator =  $("#sc-size-calc-content-1 .select-separator");
-
-    var calcUnit = {"sc-size-calc-unit-in": $("#sc-size-calc-unit-in"), 
-    "sc-size-calc-unit-cm": $("#sc-size-calc-unit-cm")};
-    var calcUnitSeparator =  $("#sc-size-calc-content-3 .select-separator");
-
-    var calcInput = $("#sc-size-calc-measure-input");
-    var calcResult = $("#sc-size-calc-result");
-
-    $("#sc-row-waist").hover(function () { womanEmpty.hide(); womanWaist.show(); },
-        function () { womanWaist.hide(); womanEmpty.show(); });
-    $("#sc-row-bust").hover(function () { womanEmpty.hide(); womanBust.show();},
-      function () { womanBust.hide(); womanEmpty.show(); });
-    $("#sc-row-hips").hover(function () { womanEmpty.hide(); womanHips.show();},
-      function () { womanHips.hide(); womanEmpty.show(); });
-
-    for ( bodypart in calcBodypart) {
-      if (calcBodypart.hasOwnProperty(bodypart)) {
-          calcBodypart[bodypart].click(function(){selectBodypart($(this).attr("id"),$(this).attr("class") );});
-      }
-    }      
-
-    for ( unit in calcUnit) {
-      if (calcUnit.hasOwnProperty(unit)) {
-          calcUnit[unit].click(function(){selectUnit($(this).attr("id"),$(this).attr("class") );});
-      }
-    }      
-
-    calcInput.focusout(function(){goToCalcStep(2);});
-
-    var selectBodypart = function(bodypartInput, clazz){
-      console.log("selectBodypart",bodypartInput, clazz);
-      if(clazz && !clazz.includes("selected")){
-        for ( bodypart in calcBodypart) {
-          if (calcBodypart.hasOwnProperty(bodypart)) {
-            if(bodypart==bodypartInput){
-              calcBodypart[bodypart].addClass('selected');
-              calcBodypart[bodypart].removeClass('unselected');
-            } 
-            else{
-              calcBodypart[bodypart].addClass('unselected');
-              calcBodypart[bodypart].removeClass('selected');
-            }
-          }
-          calcBodypartSeparator.addClass('unselected');
-        }
-      }
-      else{
-
-        for ( bodypart in calcBodypart) {
-          if (calcBodypart.hasOwnProperty(bodypart)) {
-            calcBodypart[bodypart].removeClass('unselected');
-            calcBodypart[bodypart].removeClass('selected');
-          }
-        }
-        calcBodypartSeparator.removeClass('unselected');
-        calcBodypartSeparator.removeClass('selected');
-
-      }
-
-
-      womanWaist.hide();
-      womanHips.hide();
-      womanBust.hide();
-      womanEmpty.hide(); 
-      
-      if(bodypartInput == 'sc-size-calc-bodypart-waist') {
-        womanWaist.show();
-      }
-      else if(bodypartInput == 'sc-size-calc-bodypart-hips') {
-         womanHips.show();
-        }
-      else if(bodypartInput == 'sc-size-calc-bodypart-bust') {
-        womanBust.show();
-      }
+          if(selectedSizeCell.startsWith("sc_cell_0"))
+            selectedImage = womanBust;
+          else if(selectedSizeCell.startsWith("sc_cell_1"))
+            selectedImage = womanWaist;
+          else if(selectedSizeCell.startsWith("sc_cell_2"))
+            selectedImage = womanHips;
+          else
+          selectedImage = womanEmpty;
     
-      goToCalcStep(1);
-    };
-
-    var selectUnit = function(unitInput, clazz){
-      if(clazz && !clazz.includes("selected")){
-        for ( unit in calcUnit) {
-          if (calcUnit.hasOwnProperty(unit)) {
-            if(unit==unitInput){
-              calcUnit[unit].addClass('selected');
-              calcUnit[unit].removeClass('unselected');
-            } 
-            else{
-              calcUnit[unit].addClass('unselected');
-              calcUnit[unit].removeClass('selected');
-            }
-          }
-        }
-        calcUnitSeparator.addClass('unselected');
-
-      }
-      else{
-
-        for ( unit in calcUnit) {
-          if (calcUnit.hasOwnProperty(unit)) {
-            calcUnit[unit].removeClass('unselected');
-            calcUnit[unit].removeClass('selected');
-          }
-        }
-        calcUnitSeparator.removeClass('unselected');
-        calcUnitSeparator.removeClass(' selected');
-      }
-
-      goToCalcStep(3);
-    };
+          e.stopPropagation();
+        });
     
-    var goToCalcStep = function(step){
-      for ( i=0; i < 3; i++) {
-        if(i==step){
-          steps[i].addClass("active");
-          stepsHint[i].show();
-        }
-        else{
-          steps[i].removeClass("active");
-          stepsHint[i].hide();
-        }
-      }
-      if(step==1) {
-        calcInput.focus();
-      }
-
-      calcSize();
-    };
-
-    var calcSize = function(){
-      var bodypart = $("#sc-size-calc-content-1 .selected").text();
-      var measure = calcInput.val();
-      var unit = $("#sc-size-calc-content-3 .selected").text();
-      console.log("calcSize",bodypart,measure,unit);
-
-      if(bodypart && measure && unit){
-        console.log("si");
-        calcResult.text("L");
-        stepsResultHint.show();
-      }
-      else{
-        calcResult.text("");
-        stepsResultHint.hide();
-      }
+      if(selectedSizeCell!=null)
+       $("#"+selectedSizeCell).addClass("size-selected");
 
     };
 
+    refreshTable();
+
+    var currentSize = "";
 });
--- dress-size.php
+++ dress-size.php
@@ -1,14 +1,14 @@
 <?php
 /*
 Plugin Name: Dress Size
-Version: 1.1
+Version: 1.0
 Description: Dress Size Desc
 Author: Alessandro Franceschetti / Giorgio Riccardi
 Author URI: https://www.seatoskywebsolutions.ca/
 */
 
 function dress_size_scripts_basic(){
-	// wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', array(), null, false );
+	wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', array(), null, false );
     wp_register_script('dress-size-js', plugins_url( 'dress-size.js', __FILE__ ),array( 'jquery' ), '1', true );
     wp_enqueue_script('dress-size-js');
     wp_register_style( 'dress-size-style', plugins_url( 'dress-size.css', __FILE__ ), array(), '1', 'all' );
@@ -18,31 +18,32 @@ function dress_size_scripts_basic(){
 add_action( 'wp_enqueue_scripts', 'dress_size_scripts_basic' );
 
 function html_dress_size_code($gen) {
-	echo 	'  
-	<div id="sc-panel">
-		<div id="sc-image-panel">
-			<div id="sc-sketch-image-panel">
-				<img id="sc-woman-empty" src="'.plugins_url().'/dress-size/assets/'.$gen.'-empty.jpg" />
-				<img id="sc-woman-waist" src="'.plugins_url().'/dress-size/assets/'.$gen.'-waist.jpg" style="display:none"/>
-				<img id="sc-woman-bust" src="'.plugins_url().'/dress-size/assets/'.$gen.'-bust.jpg" style="display:none"/>
-				<img id="sc-woman-hips" src="'.plugins_url().'/dress-size/assets/'.$gen.'-hips.jpg" style="display:none"/>
-			</div>
-		</div>
-		<div id="sc-size-panel">
-			<h2>Size Table</h2>
-			<table id="sc-size-table">
-				<thead>
-				<tr><th>Size</th><th>XS</th><th>S</th><th>M</th><th>L</th></tr>
-				<tr><th>US Size</th><th>0-2</th><th>4-6</th><th>8-10</th><th>12-14</th></tr>
-				</thead>
-				<tbody>
-				<tr id="sc-row-bust"><td>Bust</td><td>34 <small>in</small></td><td>36 <small>in</small></td><td>38 <small>in</small></td><td>40 <small>in</small></td></tr>
-				<tr id="sc-row-waist"><td>Waist </td><td>26 <small>in</small></td><td>28 <small>in</small></td><td>30 <small>in</small></td><td>32 <small>in</small></td></tr>
-				<tr id="sc-row-hips"><td>Hips</td><td>36 1/2 <small>in</small></td><td>38 1/2 <small>in</small></td><td>40 1/2 <small>in</small></td><td>42 1/2 <small>in</small></td></tr>
-				</tbody>
-			</table>	
-	  </div>
-  </div>';
+	echo 	'<div id="sc-panel">
+			 	<div id="sc-image-panel">
+					<div id="sc-sketch-image-panel">
+						<div id="sc-woman-empty"><img  src="'.plugins_url().'/dress-size/assets/'.$gen.'-empty.jpg" /></div>
+						<div id="sc-woman-waist" style="display:none"><img  src="'.plugins_url().'/dress-size/assets/'.$gen.'-waist.jpg"/></div>
+						<div id="sc-woman-bust" style="display:none"><img  src="'.plugins_url().'/dress-size/assets/'.$gen.'-bust.jpg"/></div>
+						<div id="sc-woman-hips" style="display:none"><img  src="'.plugins_url().'/dress-size/assets/'.$gen.'-hips.jpg"/></div>
+					</div>
+				</div>
+				<div id="sc-size-panel">
+					<div class="sc-size-table-caption">
+			  			<h2>Size Table</h2>
+			  			<div class="toggleWrapper">
+							<input class="dn" type="checkbox" id="sc-unit-toggle"/>
+							<label class="toggle" for="sc-unit-toggle"><span class="toggle__handler"></span></label>
+			  			</div>
+					</div>
+					<table id="sc-size-table"  class="sc-table-responsive wp-block-table">
+			  			<thead>
+							<tr><th>Size</th><th>XS</th><th>S</th><th>M</th><th>L</th></tr>
+			  			</thead>
+			  			<tbody id="sc-size-table-body"></tbody>
+					</table>      
+				</div>
+			</div>';
+
 }
 
 
